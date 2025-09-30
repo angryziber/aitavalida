8fd35a12e7a05a082c4432e6c29209ccd3fb2f8c
Anton Keks <anton@azib.net>
2025-09-30 22:11:03 +0300

    display all questions

--- ui/src/pages/Compass.svelte
+++ ui/src/pages/Compass.svelte
@@ -2,26 +2,26 @@
   import {t} from 'i18n'
 
   let topics = t.compass.tallinn2025
-  let topicIndex = 0
-  let questionIndex = 1
-
-  $: topic = topics[topicIndex]
-  $: question = topic.questions[questionIndex]
 
   const options = [-2, -1, 0, 1, 2]
 </script>
 
-<section class="mt-4 space-y-4">
-  <h2>{topic.name}</h2>
+<div class="max-w-xl mx-auto mb-10">
+  {#each topics as topic}
+    <section class="mt-4 space-y-4">
+      <h2>{topic.name}</h2>
 
-  <h3>{questionIndex}. {question}</h3>
-
-  <div class="flex gap-4 justify-center">
-    {#each options as option, i}
-      <label class="flex items-center gap-2">
-        <input type="radio" name="option">
-        {t.compass.options[option.toString()]}
-      </label>
-    {/each}
-  </div>
-</section>
+      {#each Object.entries(topic.questions) as [i, question]}
+        <h3>{i}. {question}</h3>
+        <div class="flex gap-4 justify-center">
+          {#each options as option, i}
+            <label class="flex items-center gap-2">
+              <input type="radio" name="option">
+              {t.compass.options[option.toString()]}
+            </label>
+          {/each}
+        </div>
+      {/each}
+    </section>
+  {/each}
+</div>
