e5c25e203ce3f25b381dbac857df4a157eeb6527
Anton Keks <anton@keks.ee>
2025-12-05 10:48:19 +0200

    switch to caddy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 mkdir -p logs
 docker logs aitavalida-aitavalida-1 2>&1 | gzip > logs/aitavalida-"$(date +%Y-%m-%d_%H:%m:%S)".log.gz
-docker compose -f docker-compose.yml -f docker-compose.deploy.yml up -d
+docker compose -f compose.yml -f compose.deploy.yml up -d
